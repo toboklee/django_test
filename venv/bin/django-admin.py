723f6e565667d0a35d5dcb47cbfd8a8ae4e209af
Tobok Lee <toboklee@Toboks-Mac-mini.local>
2017-05-19 11:06:34 -0400

    remove virtualenv

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/toboklee/Documents/codingchallenge/precious-challenge/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
